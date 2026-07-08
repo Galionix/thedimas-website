b98822276c547688062a10e9b4b04ff8fc5368bf
Dmitry Galaktionov <galionix2@gmail.com>
2026-07-08 07:11:25 +0300

    Remove project header gap

--- cypress/e2e/header-layout.cy.ts
+++ cypress/e2e/header-layout.cy.ts
@@ -5,17 +5,30 @@ const routes = [
     path: "/en",
     firstContentSelector: '[data-testid="home-hero-media"]',
   },
-  { path: "/en/projects", firstContentSelector: '[class*="cards"]' },
+  {
+    path: "/en/projects",
+    firstContentSelector: '[class*="cards"]',
+    maxHeaderGap: 1,
+  },
   {
     path: "/en/projects/Bella%20AI",
     firstContentSelector: '[class*="intro"]',
+    maxHeaderGap: 1,
   },
   {
     path: "/en/services/mvp-development",
     firstContentSelector: "main section",
   },
-  { path: "/en/blog", firstContentSelector: '[class*="intro"]' },
-  { path: "/en/blog/states", firstContentSelector: '[class*="intro"]' },
+  {
+    path: "/en/blog",
+    firstContentSelector: '[class*="intro"]',
+    maxHeaderGap: 1,
+  },
+  {
+    path: "/en/blog/states",
+    firstContentSelector: '[class*="intro"]',
+    maxHeaderGap: 1,
+  },
 ];
 
 const assertFirstContentStartsBelowHeader = (route: (typeof routes)[number]) => {
@@ -32,6 +45,13 @@ const assertFirstContentStartsBelowHeader = (route: (typeof routes)[number]) => 
         contentRect.top,
         `${route.path} first content top`
       ).to.be.at.least(headerRect.bottom - 1);
+
+      if (route.maxHeaderGap !== undefined) {
+        expect(
+          contentRect.top - headerRect.bottom,
+          `${route.path} gap below fixed header`
+        ).to.be.lessThan(route.maxHeaderGap + 1);
+      }
     });
 };
 
@@ -48,6 +68,7 @@ describe("fixed header layout", () => {
     assertFirstContentStartsBelowHeader({
       path: "/en/projects/Bella%20AI",
       firstContentSelector: '[class*="intro"]',
+      maxHeaderGap: 1,
     });
     assertFirstContentStartsBelowHeader({
       path: "/en/services/mvp-development",
